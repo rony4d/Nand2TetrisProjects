b8ef454a609fdf3a197ad226ccca2ea9c2a4ed84
rony4d <rony4d@gmail.com>
2022-05-17 08:28:15 +0100

    ChapterFour completed.

--- Revamp/ChapterFour/fill/Fill.asm
+++ Revamp/ChapterFour/fill/Fill.asm
@@ -15,31 +15,54 @@
 
 //  1. Fill up the screen 
 
-
-//  Initialize the screen and loop parameters
-@SCREEN
+// *  2. Probe Keyboard RAM section * //
+@KBD
 D=A
 
-@addr       //  Store screen base address in addr variable
-M=D
+@keyboard
+M=D                     //  Store keyboard base address in a variable 
 
-@i          //  Initialize counter for outer loop for rows to zero
-M=0
+// *  Create an infinte loop to probe keyboard* //
 
-@j          //  Initialize counter for inner loop for columns to zero
-M=0
+(KEYBOARD_LOOP)
 
-@256
-D=A
-@rows
-M=D
+    //  Initialize the screen and loop parameters
+    @SCREEN
+    D=A
 
-@32
-D=A
-@columns    
-M=D        //  There are actually 512 columns but since we are setting 16 bits to 1, in one iteration, we will have 512/16 = 32
+    @addr       //  Store screen base address in addr variable
+    M=D
 
-(LOOP_ROW)
+    @i          //  Reset row counter 
+    M=0
+
+    @256
+    D=A
+    @rows
+    M=D
+
+    @32
+    D=A
+    @columns    
+    M=D    
+
+    @keyboard
+    A=M                 //  Go to the RAM address pointed by pointer
+    D=M                 //  Now we have collected the value at the address pointed by the pointer. We can now use this value to know when a key is typed
+
+    @LOOP_ROW_BLACK
+    D;JGT               //  If value read from RAM[24576] is greater than zero then go to set the pixel colour as black 
+
+
+    @LOOP_ROW_WHITE
+    D;JEQ               //  If value read from RAM[24576] is equal to zero then go to set the pixel colour as white 
+
+    @KEYBOARD_LOOP
+    0;JMP
+
+
+
+(LOOP_ROW_BLACK)
 
     @i
     D=M
@@ -47,7 +70,7 @@ M=D        //  There are actually 512 columns but since we are setting 16 bits t
     @rows
     D=D-M
 
-    @END
+    @KEYBOARD_LOOP
     D;JEQ
     
     @i                  
@@ -56,7 +79,8 @@ M=D        //  There are actually 512 columns but since we are setting 16 bits t
     @j
     M=0                 //  reset column counter 
 
-    (LOOP_COLUMN)
+
+    (LOOP_COLUMN_BLACK)
 
         @j
         D=M
@@ -64,13 +88,13 @@ M=D        //  There are actually 512 columns but since we are setting 16 bits t
         @columns
         D=D-M
 
-        @LOOP_ROW
+        @LOOP_ROW_BLACK
         D;JEQ
 
 
         @addr           //  
-        A=M             //  Recall that addr points to a an address (i.e a pointer), so we have to go to the address pointed by the pointed and then change the value
-        M=-1            //  Set all 16 bits in the current RAM chip addressed by A to 1. This is blacken the entire screen
+        A=M             //  Recall that addr points to a an address (i.e a pointer), so we have to go to the address pointed by the pointer and then change the value
+        M=-1             //  Set all 16 bits in the current RAM chip addressed by A to value in setpixel variable. -1 for black and 0 for white
 
         
         @j              
@@ -79,15 +103,63 @@ M=D        //  There are actually 512 columns but since we are setting 16 bits t
         @addr
         M=M+1           //  Increase the address pointed by addr by one so it can point to the next RAM chip. 
 
-        @LOOP_COLUMN
+        @LOOP_COLUMN_BLACK
         0;JMP           //  Continue in this loop till we breakout
 
 
-    @LOOP_ROW
+    @LOOP_ROW_BLACK
+    0;JMP
+
+(LOOP_ROW_WHITE)
+
+    @i
+    D=M
+
+    @rows
+    D=D-M
+
+    @KEYBOARD_LOOP
+    D;JEQ
+    
+    @i                  
+    M=M+1               //  Increase row counter by 1
+
+    @j
+    M=0                 //  reset column counter 
+
+
+    (LOOP_COLUMN_WHITE)
+
+        @j
+        D=M
+
+        @columns
+        D=D-M
+
+        @LOOP_ROW_WHITE
+        D;JEQ
+
+        @addr           //  
+        A=M             //  Recall that addr points to a an address (i.e a pointer), so we have to go to the address pointed by the pointer and then change the value
+        M=0             //  Set all 16 bits in the current RAM chip addressed by A to value in setpixel variable. -1 for black and 0 for white
+
+        
+        @j              
+        M=M+1           //  Increase column counter by 1
+
+        @addr
+        M=M+1           //  Increase the address pointed by addr by one so it can point to the next RAM chip. 
+
+        @LOOP_COLUMN_WHITE
+        0;JMP           //  Continue in this loop till we breakout
+
+
+
+    @LOOP_ROW_WHITE
     0;JMP
 
 
 
-(END)
-    @END
-    0;JMP
+
+
+
